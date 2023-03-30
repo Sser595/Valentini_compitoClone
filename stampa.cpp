672a9993471ca451fc5da672774753bba7fff5f8
Serena Valentini <valentiniserena595@gmail.com>
2023-03-30 09:14:46 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+Serena Valentini 30/03
 */
 
 # include <iostream>
